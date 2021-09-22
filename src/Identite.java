da41e4847cd27c68b275ec85168bafe335623a6a
Wassim <wassim88881@outlook.fr>
2021-09-22 10:44:26 +0200

    Changement du diagramme de classe

--- src/Identite.java
+++ src/Identite.java
@@ -1,8 +1,6 @@
 /**
  * Classe qui permet de définir l'identité d'un étudiant
  */
-
-
 public class Identite{
 
     /**
@@ -16,6 +14,43 @@ public class Identite{
     /**
      * Attribut qui caractérise le prénom d'un étudiant
      */
-    private String prenom:
+    private String prenom;
 
+    /**
+     * Constructeur qui initialise les paramètres de la classe Identité
+     * @param pNip Numéro d’Identification Personnel de l'étudiant
+     * @param pNom Nom de l'étudiant
+     * @param pPrenom Prénom de l'étudiant
+     */
+    public Identite(String pNip, String pNom, String pPrenom){
+        this.nip = pNip;
+        this.nom = pNom;
+        this.prenom = pPrenom;
+    }
+
+    /**
+     * Getteur qui permet de récuperer le nip de l'étudiant
+     * @return le nip de l'étudiant en forme de String
+     */
+    public String getNip(){
+        return this.nip;
+    }
+
+    /**
+     * Getteur qui permet de récuperer le nom de l'étudiant
+     * @return le nom de l'étudiant en forme de String
+     */
+    public String getNom(){
+        return this.nom;
+    }
+
+    /**
+     * Getteur qui permet de récuperer le prénom de l'étudiant
+     * @return le prénom de l'étudiant en forme de String
+     */
+    public String getPrenom(){
+        return this.prenom;
+    }
+
+    
 }
