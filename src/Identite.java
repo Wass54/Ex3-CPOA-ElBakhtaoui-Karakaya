e71ec5dbeeaa1cccd1455591ee21e489537c4b3a
Wassim <wassim88881@outlook.fr>
2021-09-22 10:35:37 +0200

    Changement du diagramme de classe

--- src/Identite.java
+++ src/Identite.java
@@ -1,5 +1,21 @@
+/**
+ * Classe qui permet de définir l'identité d'un étudiant
+ */
+
+
 public class Identite{
 
-    
+    /**
+     * Attribut qui caractérise Numéro d’Identification Personnel
+     */
+    private String nip;
+    /**
+     * Attribut qui caractérise le nom d'un étudiant
+     */
+    private String nom;
+    /**
+     * Attribut qui caractérise le prénom d'un étudiant
+     */
+    private String prenom:
 
 }
