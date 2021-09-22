ac03d475072da5440503c70052d636b797857182
Wassim <wassim88881@outlook.fr>
2021-09-22 11:00:27 +0200

    Ajout de la classe Etudiant pas fini

---
+++ src/Etudiant.java
@@ -0,0 +1,20 @@
+/**
+ * Classe qui permet de définir un étudiant
+ */
+
+public class Etudiant{
+
+    /**
+     * Attribut qui permet de connaitre l'identité de l'étudiant
+     */
+    private Identite identite;
+    /**
+     * Attribut qui permet de connaitre la formation de l'étudiant
+     */
+    private Formation formation;
+    /**
+     * Attribut qui permet de savoir les notes de l'étudiant
+     */
+    private Map<String,ArrayList<Float>> notes;
+
+}
