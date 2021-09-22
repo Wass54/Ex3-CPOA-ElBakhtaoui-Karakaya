770f86e640fa2c403c24ee45372412fbbbab98cc
Karakaya Enes <enes.karakaya5@etu.univ-lorraine.fr>
2021-09-22 11:33:22 +0200

    ok

--- src/Formation.java
+++ src/Formation.java
@@ -2,27 +2,49 @@ public class Formation{
 
     /**
      * String id correspond à l'id de l'étudiant
-     * Map<String,Integer> 
+     * Map<String,Integer> est un hashcode matiere avec son coefficient
      * 
      */
     private String id;
     private Map<String,Integer> matiere;
 
+
+    /**
+     * @param a nom de l'étudiant
+     * @param b hahscode matiere
+     * 
+     */
     public Formation(String a, Map<String,Integer> b){
         this.id = a;
         this.matiere = b;
     }
 
+
+    /**
+     * @return id de l'étudiant
+     */
     public String getId(){
         return this.id;
     }
 
+    /**
+     * @param a string matiere
+     * @param b string coefficient
+     * On ajoute une matiere
+     * 
+     */
     public void ajouterMatiere(String a, int b){
         if(this.containsKey(a)){
         matiere.put(a, b);
         }
     }
 
+
+        /**
+     * @param a string matiere
+     * On supprime une matiere
+     * 
+     */
     public void supprimerMatiere(String a){
         if(this.containsKey(a)){
             this.remove(a);
@@ -31,6 +53,11 @@ public class Formation{
 
 
 
+    /**
+     * @param a le nom de l'étudiant
+     * @return int le noef de la matiere
+     * on accède la valeur ( coefficient) de la matiere donne en parametre
+     */
     public int coefMatiere(String a){
         int coef = null;
         coef = this.get(a);
@@ -39,18 +66,4 @@ public class Formation{
         }
         return coef;
     }
-
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Map<String,Integer> getMatiere() {
-        return this.matiere;
-    }
-
-    public void setMatiere(Map<String,Integer> matiere) {
-        this.matiere = matiere;
-    }
-
 }    
