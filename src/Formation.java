34bd4b9511ef2e8fb7f35fe9ffae11d8a5ac5e98
Karakaya Enes <enes.karakaya5@etu.univ-lorraine.fr>
2021-09-22 10:37:54 +0200

    initialisation 2

--- src/Formation.java
+++ src/Formation.java
@@ -2,5 +2,26 @@ public class Formation{
     private String id;
     private Map<String,integer> matiere;
 
-    public Formation(String a, )
+    public Formation(String a, Map<String,integer> b){
+
+        this.id = a;
+        this.matiere = b;
+    }
+
+    public String getId(){
+
+    }
+
+    public void ajouterMatiere(String a){
+
+    }
+
+    public void supprimerMatiere(String a){
+
+
+    }
+
+    public int coefMatiere(String a){
+
+    }
 }
